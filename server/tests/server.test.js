9d72e9a76518eaeeef55286ae4fbca0c50bf320b
sanngly <esanjoyganguly@gmail.com>
2017-12-18 18:11:21 +0530

    Add test for GET/todos routes by sanngly

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,18 @@ const request = require('supertest');
 var {app} = require('./../server');
 var {Todo} = require('./../models/todo');
 
+const todos = [{
+    text: 'Call Police 100'
+}, {
+    text: 'Switch off the power and lights'
+}, {
+    text: 'Text message to friends and neighbours'
+}];
+
 beforeEach((done) => {
     Todo.remove({}).then(() => {
+        Todo.insertMany(todos);
+    }).then(() => {
         done();
     });
 });
@@ -24,7 +34,7 @@ describe('POST /todos', () => {
             if (err) {
                 return done(err);
             }
-            Todo.find().then((todos) => {
+            Todo.find({text}).then((todos) => {
                 expect(todos.length).toBe(1);
                 expect(todos[0].text).toBe(text);
                 done();
@@ -45,13 +55,35 @@ describe('POST /todos', () => {
                 return done(err);
             }
             Todo.find().then((todos) => {
-                expect(todos.length).toBe(0);
+                expect(todos.length).toBe(3);
                 done();
             }).catch((e) => {
                 done(e);
             });
         });
     });
+});
+
+describe('GET /todos', () => {
+    it('should get all todos from mongo db', (done) => {
+        request(app)
+        .get('/todos')
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todos.length).toBe(3);            
+        })
+        .end((err, res) => {
+            if (err) {
+                return done(err);
+            }
+            Todo.find().then((todos) => {
+                expect(todos.length).toBe(3);
+                done();
+            }).catch((e) => {
+                done(e);
+            });
+        });
+    })
 });
 
 
