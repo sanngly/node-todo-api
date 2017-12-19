67b8e978d66538db69e746d4ea55a42453a5d8a0
sanngly <esanjoyganguly@gmail.com>
2017-12-19 14:13:56 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 var bodyParser = require('body-parser');
 const port = process.env.port || 3000;
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
@@ -27,6 +28,25 @@ app.get('/todos', (req, res) => {
         res.send({todos});
     }, (err) => {
         res.status(400).send(err);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    // console.log(req.params.id);
+    // res.send(req.params);
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)){
+        return res.status(404).send('Invalid id.');
+    }
+    Todo.findById(id).then((todo) => {
+        if(!todo) {
+            console.log('Unable to find the Todo by Id.');
+            return res.status(404).send();
+        }
+        res.status(200).send({todo});
+    }).catch((err) => {
+        console.log('Error occured while getting todo by Id', err);
+        res.status(404).send(err);
     });
 });
 
