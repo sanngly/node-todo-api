1242c463569c6238b7a201fcac3da608bf70e401
sanngly <esanjoyganguly@gmail.com>
2017-12-21 17:34:17 +0530

    Hashing the password using mongoose middleware

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,7 +1,20 @@
 const {SHA256} = require('crypto-js');
+const bcrypt = require('bcryptjs');
+
+var password = 'Qwety@1234';
 
 var text = "I am sanjoy ganguly";
 var hash = SHA256(text).toString();
+
+/* bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) =>{
+        console.log('Hashed Value ', hash);
+    })
+}); */
+var hashedValue = '$2a$10$ZI5pHz31NLQ2PssCJD42U.zFxaYlmEGBk42UGF4/S6NNIU16D.o7K';
+bcrypt.compare(password, hashedValue, (err, result)=>{
+    console.log(result);
+});
 
 console.log(`Mesasge: ${text}`);
 console.log(`Hash: ${hash}`);
